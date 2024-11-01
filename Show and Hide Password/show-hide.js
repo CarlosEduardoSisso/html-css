21a1af087725a1654868ebf508fed3e5625abc4e
Carlos Eduardo Sisso <carloseduardossz@gmail.com>
2024-11-01 18:38:02 -0300

    Mostrar e Esconder Senha
    Input de senha com opção de mostrar e esconder em HTML, CSS e JavaScript.

---
+++ Show and Hide Password/show-hide.js
@@ -0,0 +1,11 @@
+function showHide() {
+    let inputPass = document.getElementById('senha');
+    let btnShowPass = document.getElementById('btn-senha');
+    if (inputPass.type === 'password') {
+        inputPass.setAttribute('type', 'text')
+        btnShowPass.classList.replace('bi-eye-fill', 'bi-eye-slash-fill')
+    } else {
+        inputPass.setAttribute('type', 'password')
+        btnShowPass.classList.replace('bi-eye-slash-fill', 'bi-eye-fill')
+    }
+}
